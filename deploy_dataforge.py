d1f6781465090200c01fc90b881675d30700020f
ScR4PP <95632351+JarcauCristian@users.noreply.github.com>
2024-04-10 11:26:17 +0300

    Making executalbe

--- deploy_dataforge.py
+++ deploy_dataforge.py
@@ -27,6 +27,11 @@ def undeploy(path: str, **kwargs):
     return result.returncode, result.stdout, result.stderr
 
 
+def make_executable(path: str):
+    result = subprocess.run(["chmod", "+x", str(path)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.returncode, result.stdout, result.stderr
+
+
 def load_config(path: str):
     with open(path, "r") as f:
         config = json.loads(f.read())
@@ -37,9 +42,12 @@ def load_config(path: str):
 def main():
     config = load_config("./config.json")
 
+    for path in Path.cwd().rglob("*.sh"):
+        make_executable(path)
+
     failed_deployments = []
     for path in Path.cwd().rglob("deploy_*.sh"):
-        deployment = str(path).split("\\")[-2]
+        deployment = str(path).split("/")[-2]
         print(path)
         if config[deployment].get("wait_time") is not None:
             result = deploy(path, **{"namespace": config["namespace"]} | config[deployment]["values"]) if config[deployment].get("values") is not None else deploy(path, **{"namespace": config["namespace"]})
@@ -52,7 +60,7 @@ def main():
             failed_deployments.append(deployment)
     
     for path in Path.cwd().rglob("undeploy_*.sh"):
-        deployment = str(path).split("\\")[-2]
+        deployment = str(path).split("/")[-2]
         if deployment in failed_deployments:
             undeploy(path, **{"namespace": config["namespace"]})
 
